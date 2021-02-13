f9f049c7bc5677c2973887ae3e6d791290e1f647
Cristian Delcid <cristian.delcid10@gmail.com>
2021-02-12 20:51:21 -0600

    feature: Layout page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Layout />
     </div>
   );
 }
