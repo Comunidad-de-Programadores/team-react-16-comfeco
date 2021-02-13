95064c016a5e4c7a5c767aeb698ba73274427de9
Cristian Delcid <cristian.delcid10@gmail.com>
2021-02-12 20:26:28 -0600

    feature: Header/Footer added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+            <figure>
+                <img src='/icons/logo.svg' alt='logo'/>
+            </figure>
+        </header>
+    )
+}
