7e1811f02cf0dac8e93e88fa8abb709d3951bf30
Jimena-chan <jiimenagramajo@gmail.com>
2021-02-17 20:24:57 -0600

    header bug fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/img/logo.svg";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <header>
-      <img src="/icons/logo.svg" alt="logo" />
+      <Link to="/">
+        <img src={logo} alt="logo" draggable="false" className="logo" />
+      </Link>
     </header>
   );
 };
