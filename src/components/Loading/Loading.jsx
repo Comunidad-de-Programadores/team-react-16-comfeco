52d6ff13c162b6fe6d83d48eeae197980d486677
Jimena-chan <jiimenagramajo@gmail.com>
2021-02-24 14:22:06 -0600

    adding files animation loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Loading.scss";
+import Loader from "../Loader/Loader";
+
+function Loading() {
+  return (
+    <div className="Loading">
+      <Loader />
+    </div>
+  );
+}
+
+export default Loading;
