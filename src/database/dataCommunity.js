c5ba97fc2f6e6d8bcafcbab0e4bba4c81a570e3a
Jimena-chan <jiimenagramajo@gmail.com>
2021-02-28 16:46:52 -0600

    finished component community #27

---
+++ src/database/dataCommunity.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    name: "Comunidad de Programadores",
+    url: "https://discord.gg/5Bb5yvzNPr",
+    image:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Ffae49dcf-7e01-40dd-a435-adcbf56f72c0%2Fnew_Imagen_Programador.png?table=block&id=edc76893-6add-402f-a435-48c2d6837f26&width=2730&userId=&cache=v2",
+  },
+  {
+    id: 2,
+    name: "El lenguaje de los programadores",
+    url: "https://www.facebook.com/lenguajeprogramador",
+    image:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F251de6e9-7154-4823-8646-1fd227f42a4b%2Fimage0.jpg?table=block&id=d2272fbf-3ff5-4030-86d6-0b408c8e17e2&width=2730&userId=&cache=v2",
+  },
+  {
+    id: 3,
+    name: "Develoteca",
+    url: "https://discord.gg/pSmj5RKPYm",
+    image:
+      "https://yt3.ggpht.com/ytc/AAUvwnjn2SzlPdAe63iXJOfkoD7gsdbQ4OftxWAuquIdPA=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: 4,
+    name: "Undefined Devs",
+    url: "https://undefineddevs.com/",
+    image:
+      "https://undefineddevs.com/wp-content/uploads/2020/08/cropped-logo-undefinedDevs-isotipo.png",
+  },
+];
