48847ba3f532f304719d5dcbe2d7e462078977c0
Jimena-chan <jiimenagramajo@gmail.com>
2021-03-04 19:52:12 -0600

    starting component and design #39

---
+++ src/components/ListEvents/ListEvents.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./ListEvents.scss";
+import data from "../../database/dataEvents";
+
+const ListEvents = () => {
+  const [eventos, setEventos] = useState(data);
+
+  return (
+    <>
+      <div className="events--title">
+        <h3>Eventos</h3>
+      </div>
+      {eventos.map((event) => {
+        const { id, name, url, image } = event;
+        return (
+          <article key={id} className="event">
+            <h4>{name}</h4>
+            <img src={image} alt={name} />
+            <p>
+              <a href={url} target="blank">
+                Unirse
+              </a>
+            </p>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default ListEvents;
--- src/components/TabsContainers/MyProfile/MyProfile.jsx
+++ src/components/TabsContainers/MyProfile/MyProfile.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import "./MyProfile.scss";
+import ListEvents from "../../ListEvents/ListEvents";
 
 const MyProfile = () => {
   return (
     <>
-      <p>My profile</p>
+      <div className="myprofile--container">
+        <div className="myprofile--top">
+          <div className="top--profile">
+            <p>Sección Banner Profile</p>
+          </div>
+        </div>
+        <div className="myprofile--bottom">
+          <div className="bottom--left">
+            <div className="left--activity">
+              <p>Sección Actividad Reciente</p>
+            </div>
+          </div>
+          <div className="bottom--right">
+            <div className="right--insignias">
+              <p>Sección Insignias</p>
+            </div>
+            <div className="right--eventos">
+              <ListEvents />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
