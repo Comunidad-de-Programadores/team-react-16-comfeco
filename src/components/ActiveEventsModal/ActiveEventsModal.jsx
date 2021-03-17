f6ef349eae738ef4136c8897ecd01b80c32c634d
Cristian Delcid <cristian.delcid10@gmail.com>
2021-03-16 20:53:39 -0600

    refactor: modal updated

--- src/components/ActiveEventsModal/ActiveEventsModal.jsx
+++ src/components/ActiveEventsModal/ActiveEventsModal.jsx
@@ -9,40 +9,44 @@ export const ActiveEventsModal = ({ setShowModal, showModal, eventSelected }) =>
    const { title, imageUrl, small_description } = eventSelected;
 
    return (
-      <div className='active-modal'>
-         <section
-            className='modal'
-            title={ title }
-         >
-            <div className='modal-info'>
-               <h2>
-                  Bienvenid@ a { title }
-               </h2>
-               <p>
-                  { small_description }
-               </p>
-            </div>
-            <div className='modal-image'>
-               <img src={ imageUrl } alt={ title }/>
-
-            </div>
-         </section>
-
-         <p
-            className='success-register'
-         >
-            <IoIosCheckmarkCircleOutline />
-            Registro exitoso a { title }
-         </p>
-
-         <a
-            href="#!"
-            className='close-modal'
+      <>
+         <div className='backdrop'
             onClick={ () => setShowModal( !showModal ) }
-            title='Cerrar'
-         >
-            <AiFillCloseCircle />
-         </a>
-      </div>
+         ></div>
+         <div className='active-modal'>
+            <section
+               className='modal'
+               title={ title }
+            >
+               <div className='modal-info'>
+                  <h2>
+                     Bienvenid@ a { title }
+                  </h2>
+                  <p>
+                     { small_description }
+                  </p>
+               </div>
+               <div className='modal-image'>
+                  <img src={ imageUrl } alt={ title }/>
+               </div>
+            </section>
+
+            <p
+               className='success-register'
+            >
+               <IoIosCheckmarkCircleOutline />
+               Registro exitoso a { title }
+            </p>
+
+            <a
+               href="#!"
+               className='close-modal'
+               onClick={ () => setShowModal( !showModal ) }
+               title='Cerrar'
+            >
+               <AiFillCloseCircle />
+            </a>
+         </div>
+      </>
    )
 }
